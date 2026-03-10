040f72209f675bf0648e6436576969d12ea573fe
Aldahir Alonso <aldahir0603@hotmail.com>
2026-03-10 17:06:49 -0600

    Creando Endpoint Post

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from fastapi import FastAPI
-app = FastAPI()
+app = FastAPI(title="N8N_Python")
+
+class IncomingData(BaseModel):
+    text: str
+    source: str | None = None
+
+@app.post("/analyze")
+def analyze(data: IncomingData):
+    return {"received_text": data.text, "length": len(data.text)}
+
+@app.get("/")
+def health_check():
+    return {"status": "ok", "message": "API funcionando!"}
